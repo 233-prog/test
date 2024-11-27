c1f3d6b4839f0d239c5e59521c9cb1879af7df8d
Advay <advaykajaria@gmail.com>
2024-11-27 17:25:41 +0530

    .

--- AP/Userflow_new.py
+++ AP/Userflow_new.py
@@ -13,5 +13,16 @@ def main():
     if len(value) <= 1 or len(value) >= 8:
         print("Invalid input. The value must be between 1 and 8 in length.")
         return
+    
+def validate_binary(value):
+    return value(value) and all(char in '01' for char in value)
+
+def validate_decimal(value):
+    return value(value) and all('0' <= char <= '9' for char in value)
+
+def validate_hex(value):
+    return value(value) and all(char in "0123456789ABCDEFabcdef" for char in value)
+
+
 if __name__ == "__main__":
     main()
