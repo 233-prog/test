0546562cb0a2f44a77939735ad5bb9d0c38215a3
Advay <advaykajaria@gmail.com>
2024-11-27 18:49:31 +0530

    .

--- AP/Userflow_new.py
+++ AP/Userflow_new.py
@@ -14,7 +14,35 @@ def main():
         print("Invalid input. The value must be between 1 and 8 in length.")
         return
     
-def validate_binary(value):
+
+
+if from_NS == 1:  
+        if to_NS == 2: 
+            result = convert_binary_to_decimal(value)
+        elif to_NS == 3: 
+            result = convert_binary_to_hex(value)
+        else:
+            result = value  
+    elif from_NS == 2:  
+        if to_NS == 1:  
+            result = convert_decimal_to_binary(value)
+        elif to_NS == 3: 
+            result = convert_decimal_to_hex(value)
+        else:
+            result = value  
+    elif from_NS == 3:  
+        if to_NS == 1:  
+            result = convert_hex_to_binary(value)
+        elif to_NS == 2:  
+            result = convert_hex_to_decimal(value)
+        else:
+            result = value 
+
+if __name__ == "__main__":
+    main()
+
+exit()
+    def validate_binary(value):
     return value(value) and all(char in '01' for char in value)
 
 def validate_decimal(value):
@@ -23,6 +51,14 @@ def validate_decimal(value):
 def validate_hex(value):
     return value(value) and all(char in "0123456789ABCDEFabcdef" for char in value)
 
+def convert_binary_to_decimal(value):
+  
+def convert_decimal_to_binary(value):
+   
+def convert_decimal_to_hex(value):
+   
+def convert_hex_to_decimal(value):
 
-if __name__ == "__main__":
-    main()
+def convert_binary_to_hex(value): 
+
+def convert_hex_to_binary(value):
