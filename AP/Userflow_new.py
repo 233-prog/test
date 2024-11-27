5b95c2fccb3ca4813111d2814359a148940206d5
Advay <advaykajaria@gmail.com>
2024-11-27 16:37:21 +0530

    .

--- AP/Userflow_new.py
+++ AP/Userflow_new.py
@@ -1,15 +1,17 @@
 def main():
-    from_NS= input("Choose the current Number System (1 - Binary, 2 - Decimal, 3 - Hexadecimal): ")
+    from_NS= int(input("Choose the current Number System (1 - Binary, 2 - Decimal, 3 - Hexadecimal): "))
     if from_NS not in [1, 2, 3]:
         print("Invalid choice.")
         return
 
-    to_NS = input("Choose the desired Number System (1 - Binary, 2 - Decimal, 3 - Hexadecimal): ")
+    to_NS = int(input("Choose the desired Number System (1 - Binary, 2 - Decimal, 3 - Hexadecimal): "))
     if to_NS not in [1, 2, 3]:
         print("Invalid choice.")
         return
 
     value = input("Enter the value to be converted: ")
-    if len(value) < 1 or len(value) > 8:
+    if len(value) <= 1 or len(value) >= 8:
         print("Invalid input. The value must be between 1 and 8 in length.")
         return
+if __name__ == "__main__":
+    main()
