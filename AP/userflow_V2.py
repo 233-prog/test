281e8500da5902ce54565592f06542034c8ea3a0
Advay <advaykajaria@gmail.com>
2024-12-03 14:33:32 +0530

    Update userflow_V2.py

--- AP/userflow_V2.py
+++ AP/userflow_V2.py
@@ -56,8 +56,14 @@ def convert_hex_to_decimal(value):
         position = position + 1
     return decimal_value
 
-def convert_binary_to_hex(value):   
-        pass
+def convert_binary_to_hex(binary_str):  
+        HEX_TO_BINARY = {'0': "0000", '1': "0001", '2': "0010", '3': "0011", '4': "0100", '5': "0101", '6': "0110", '7': "0111",'8': "1000", '9': "1001", 'A': "1010", 'B': "1011", 'C': "1100", 'D': "1101", 'E': "1110", 'F': "1111"} 
+        hex_digits = []
+        while binary_str:
+            hex_digit = int(binary_str[:4], 2)
+            hex_digits.append(hex(hex_digit)[2:].upper())
+            binary_str = binary_str[4:]
+        return ''.join(hex_digits)
 
 def convert_hex_to_binary(value):
     HEX_TO_BINARY = {'0': "0000", '1': "0001", '2': "0010", '3': "0011", '4': "0100", '5': "0101", '6': "0110", '7': "0111", '8': "1000", '9': "1001", 'A': "1010", 'B': "1011", 'C': "1100", 'D': "1101", 'E': "1110", 'F': "1111"}
