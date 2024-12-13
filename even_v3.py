578a266e6ff032e43a8273d274d30f897f0cd216
Advay <advaykajaria@gmail.com>
2024-12-13 13:42:39 +0530

    .

--- even_v3.py
+++ even_v3.py
@@ -1,14 +1,13 @@
 def even(value):
     if value == 0:
         return True
-
+    
     while value > 0:
         value = value - 2
-
+        
     return value == 0
 
 number = int(input("Enter an integer: "))
-
 if even(number):
     print(number, "is even.")
 else:
