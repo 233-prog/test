0691b2d998d04f215d9ae9242aa88fd5d3a2febc
Advay <advaykajaria@gmail.com>
2024-12-13 13:40:35 +0530

    .

---
+++ even_v3.py
@@ -0,0 +1,15 @@
+def even(value):
+    if value == 0:
+        return True
+
+    while value > 0:
+        value = value - 2
+
+    return value == 0
+
+number = int(input("Enter an integer: "))
+
+if even(number):
+    print(number, "is even.")
+else:
+    print(number, "is odd.")
