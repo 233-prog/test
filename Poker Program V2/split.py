c63fcca4e1705f12048ad0c3ba09ca646531e4e4
Advay <advaykajaria@gmail.com>
2025-01-21 15:52:30 +0530

    .

--- Poker Program V2/split.py
+++ Poker Program V2/split.py
@@ -5,8 +5,18 @@ board_cards_input = input("Enter the board cards: ")
 user_cards_list = user_cards_input.split()
 board_cards_list = board_cards_input.split()
 
-user_card_tuples = [(card[:-1], card[-1]) for card in user_cards_list]
-board_card_tuples = [(card[:-1], card[-1]) for card in board_cards_list]
+user_card_tuples = []
+for card in user_cards_list:
+    value = card[:-1] 
+    suit = card[-1]    
+    user_card_tuples.append((value, suit))  
+
+
+board_card_tuples = []
+for card in board_cards_list:
+    value = card[:-1]  
+    suit = card[-1]  
+    board_card_tuples.append((value, suit))  
 
 print("User Cards:")
 print(user_card_tuples)
