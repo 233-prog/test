3fbd660cc510f901cae10fe51b929c09f09848a8
Advay <advaykajaria@gmail.com>
2025-01-27 16:29:52 +0530

    .

--- Poker Program V2/Input_tuple.py
+++ Poker Program V2/Input_tuple.py
@@ -7,56 +7,82 @@ def get_user_cards(card_type, needed_cards):
         cards_list = user_input.split()
 
         if len(cards_list) != needed_cards:
-            print(f" You need to enter exactly {needed_cards} cards. Try again!")
+            print(f" You need to enter {needed_cards} cards. Try again!")
             continue
         
         is_valid = True
         cards = set()
-        
-        for card in cards:
+
+        for card in cards_list:
             if len(card) != 2:
                 is_valid = False
                 break
 
-            first_char, second_char = card[0], card[1]
+            first_char = card[0]
+            second_char = card[1]
 
-            if first_char not in valid_ranks or second_char not in valid_suits:
+            if first_char not in valid_ranks:
+                is_valid = False
+                break
+            if second_char not in valid_suits:
                 is_valid = False
                 break
 
             if card in cards:
-                print("Please try again with different cards.")
+                print("Duplicate card found in user input. Please try again with different cards.")
                 is_valid = False
                 break
-            
+
             cards.add(card)
 
         if is_valid:
             return [tuple(card) for card in cards_list]
 
-        print("Something went wrong. Make sure your cards are in the right format like XY XY.")
+        print("Make sure your cards are in the right format like XY XY.")
 
 def get_community_cards():
     valid_numbers = [0, 3, 4, 5]
     community_cards = []
 
-    community_input = input("Enter the community cards: ").strip()
+    while True:
+        community_input = input("Enter the community cards: ").strip()
 
-    if community_input:
-        community_list = community_input.split()
+        if community_input:
+            community_list = community_input.split()
 
-        if len(community_list) in valid_numbers:
-            community_cards = [tuple(card) for card in community_list]
+            if len(community_list) in valid_numbers:
+                community_card_tuples = []
+
+                for card in community_list:
+                    community_card_tuples.append(tuple(card))
+
+                community_cards = community_card_tuples
+                return community_cards
+            else:
+                print("The number of community cards must be 0, 3, 4, or 5. Please try again.")
+
         else:
-            print("The number of community cards must be 0, 3, 4, or 5. Try again.")
-            return get_community_cards()
+            return community_cards
 
-    return community_cards
+def validate_no_duplicates(user_cards, community_cards):
+    all_cards = user_cards + community_cards
+    seen_cards = set()
+
+    for card in all_cards:
+        if card in seen_cards:
+            return False  
+        seen_cards.add(card)
+    
+    return True
 
 def main():
     user_cards = get_user_cards("user", 2)
     board_cards = get_community_cards()
-    
+
+    if not validate_no_duplicates(user_cards, board_cards):
+        print("Duplicate cards detected. Please try again.")
+        return
+
     print(f"Your cards are: {user_cards}")
     print(f"Community cards are: {board_cards}")
 
